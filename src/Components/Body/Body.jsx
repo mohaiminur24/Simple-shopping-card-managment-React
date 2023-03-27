ca299f52e0e3f4ea88a57778e4f10f6582874acb
DeveloperMMR <mohaiminur24@gmail.com>
2023-03-27 16:11:20 +0600

    complete

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -6,11 +6,12 @@ const Body = () => {
     const [cartData, setCardData] = useState([]);
     const [product, setProduct] = useState([]);
     const [localdata, setLocaldata] = useState();
+    const [toggle, setToggle] = useState(false);
     useEffect(()=>{
         let localStorageData = localStorage.getItem('cart');
         localStorageData = JSON.parse(localStorageData);
         setLocaldata(localStorageData);
-    },[cartData])
+    },[cartData,toggle])
     useEffect(()=>{
         fetch('../../../public/products.json').then(res=>res.json()).then(data=>setProduct(data));
     },[])
@@ -30,22 +31,27 @@ const Body = () => {
         };
         
     };
+    const filterLocalData =(id)=>{
+            const findout = localdata.filter(data => data.id != id);
+            const newarray = JSON.stringify(findout);
+            localStorage.setItem('cart',newarray);
+            setToggle(!toggle)
+    }
     
     return (
         <div className='grid grid-cols-5 w-11/12 mx-auto gap-5 my-5'>
             <div className='col-span-4 grid grid-cols-3 gap-5'>{product && product.map(product => <Product funnctions={setdata} key={product.id} product={product}></Product>)}</div>
 
 
-
             <div className='border h-fit p-5 rounded-md'>
                 <h1 className='text-center font-semibold text-sm border-b pb-2'>Your Cart Summary</h1>
                 <div className='my-5'>
-                    <h1 className='font-semibold border px-2 py-1 rounded-md mb-3 w-fit'>Total items: <span className='text-sm'>{localdata && localdata.length}</span></h1>
+                    <h1 className='font-semibold border px-2 py-1 rounded-md mb-3 w-fit'>Total items: <span className='text-sm'>{localdata ? localdata.length:0}</span></h1>
 
-                    <div className='text-xs'>{localdata && localdata.map(data=> <Cart cartData={data}></Cart>)}</div>
+                    <div className='text-xs'>{localdata && localdata.map(data=> <Cart cartData={data} filterdata ={filterLocalData}></Cart>)}</div>
                 </div>
 
-                <h1 className='border-t pt-2'>Total Price: ${totalMoney? totalMoney: 0}</h1>
+                <h1 className='border-t pt-2 font-bold'>Total Price:<span className='mx-5 font-thin'>${totalMoney? totalMoney: 0}</span></h1>
             </div>
             
         </div>
--- src/Components/Body/Cart.jsx
+++ src/Components/Body/Cart.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const Cart = (props) => {
-    const {name,price} = props.cartData;
+    const filterdata = props.filterdata;
+    
+    const {name,price,id} = props.cartData;
     return (
-        <table className='table-auto text-left w-full'>
+        <table className='w-full my-2'>
             <tbody>
-                <tr>
-                    <td>{name.length > 10 ? name.slice(0,15):name}</td>
-                    <td className='text-right'>${price}</td>
+                <tr className='grid grid-cols-4'>
+                    <td className='col-span-2'>{name.length > 10 ? name.slice(0,15):name}</td>
+                    <td className='text-right text-yellow-900'> ${price}</td>
+                    <td className='text-right'>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>filterdata(id)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 mx-3 h-5 cursor-pointer hover:text-red-700">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                    </td>
                 </tr>
             </tbody>
         </table>
